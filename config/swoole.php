0985ae995a15d679e4cb10fe7503a2c09dd2da20
Roman Zaycev <box@romanzaycev.ru>
2023-03-20 23:26:27 +0700

    WIP: Docker environment

--- config/swoole.php
+++ config/swoole.php
@@ -3,32 +3,33 @@
 use DI\Container;
 use DI\ContainerBuilder;
 use Olifanton\DemoWallet\Http\Server\OpenSwoole\ServerFactory;
+use OpenSwoole\Runtime;
+use OpenSwoole\Util;
+use Psr\Log\LoggerInterface;
 
 co::set([
-    'hook_flags' => OpenSwoole\Runtime::HOOK_CURL,
+    'hook_flags' => Runtime::HOOK_CURL | Runtime::HOOK_TCP,
 ]);
 
 return static function (ContainerBuilder $builder) {
     $builder->addDefinitions([
         ServerFactory::class => static function (Container $container) {
             $config = [
-                'host' => $_ENV["HTTP_LISTEN_HOST"],
+                'host' => "0.0.0.0",
                 'port' => $_ENV["HTTP_LISTEN_PORT"],
                 'mode' => SWOOLE_PROCESS,
                 'settings' => [
-                    'worker_num' => OpenSwoole\Util::getCPUNum() * 2,
+                    'worker_num' => Util::getCPUNum() * 2,
                     'document_root' => ROOT_DIR . '/public',
                     'enable_static_handler' => true,
                     'http_index_files' => ['index.html']
                 ],
             ];
 
-            $instance = new ServerFactory(
+            return new ServerFactory(
                 $config,
-                $container->get(\Psr\Log\LoggerInterface::class),
+                $container->get(LoggerInterface::class),
             );
-
-            return $instance;
         },
     ]);
 };
---
+++ dockerfiles/backend.Dockerfile
@@ -0,0 +1,23 @@
+FROM php:8.2-cli
+
+RUN apt-get update && apt-get install -y libpng-dev openssl libssl-dev libcurl4-openssl-dev wget git
+
+RUN docker-php-ext-install gd sockets
+
+RUN cd /tmp && git clone https://github.com/openswoole/ext-openswoole.git && \
+    cd ext-openswoole && \
+    git checkout v22.0.0 && \
+    phpize  && \
+    ./configure --enable-openssl --enable-hook-curl --enable-http2 --enable-sockets && \
+    make && make install
+
+RUN touch /usr/local/etc/php/conf.d/openswoole.ini && \
+    echo 'extension=openswoole.so' > /usr/local/etc/php/conf.d/zzz_openswoole.ini
+
+RUN wget -O /usr/local/bin/dumb-init https://github.com/Yelp/dumb-init/releases/download/v1.2.2/dumb-init_1.2.2_amd64
+RUN chmod +x /usr/local/bin/dumb-init
+
+RUN apt-get autoremove -y && rm -rf /var/lib/apt/lists/*
+
+ENTRYPOINT ["/usr/local/bin/dumb-init", "--", "php"]
+WORKDIR "/var/demo-wallet"
